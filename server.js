560b337a7c1eb6c6fb2474022c3813be1f7c5f4b
PssProjekt3DTisk <125122584+PssProjekt3DTisk@users.noreply.github.com>
2023-02-28 21:54:52 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -5,19 +5,50 @@ const server = express();
 const path = require('path');
 const PORT = 3000;
 
-server.use(express.static('public'));
+server.use('*/css',express.static('public/css'));
+server.use('*/js',express.static('public/js'));
+server.use('*/img',express.static('public/img'));
+server.use(express.static(__dirname+ '/public'));
 
 server.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname+'/web page', '/index.html'));
+    res.sendFile(path.join(__dirname+ '/index.html'));
   });
 
 server.listen(PORT, function(error) {
     if(error) {
         console.log("Something went wrong ", error);
     } else {
-        console.log("Server is listening on port " + PORT)
+        console.log("Server is listening on port " + PORT);
     }
 });
 /*
 server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+*/
+
+/*
+const http = require("http");
+const fs = require("fs");
+const port = 3000;
+
+const server = http.createServer(function(req, res) {
+    fs.readFile('index.html', function(error, data) {
+        if(error) {
+            res.writeHead(404);
+            res.write("Error: File not Found");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data);
+        }
+    });
+    res.write("ahoj");
+    res.end();
+})
+
+server.listen(port, function(error) {
+    if(error) {
+        console.log("Something went wrong ", error);
+    } else {
+        console.log("Server is listening on port " + port)
+    }
+});
 */
